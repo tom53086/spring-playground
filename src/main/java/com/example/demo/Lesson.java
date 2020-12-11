47191ef8af506c44b1ea60e1b909dac0f30fad24
Tom Thorpe <tom53086@gmail.com>
2020-12-11 08:12:06 -0800

    added database to spring

--- src/main/java/com/example/demo/Lesson.java
+++ src/main/java/com/example/demo/Lesson.java
@@ -2,11 +2,7 @@ package com.example.demo;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
